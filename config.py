30bd1709555ab8ef02c4140ddce530faf8480391
Sukanya0712 <sukanyasingh656@gmail.com>
2024-07-27 18:22:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 from os import environ
 
 #TG Credentials
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
+API_ID = int(environ.get('API_ID', '5010466936'))
+API_HASH = environ.get('API_HASH', 'f8b380e48c66c9ce9c30eb8e4e3da550')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6677598838:AAFYfpf7wyEy8wcSDOAwJxvoMMmzwchoq6E')
 
 #Website Credentials
-API_KEY = environ.get('API_KEY', '5f82d8793633efb1268e8a4d3e7b4911e217d20a')
-API_URL = environ.get('API_URL', 'https://pandaznetwork.com/api')
-WEB_NAME = environ.get('WEB_NAME', 'PandazNetwork')
+API_KEY = environ.get('API_KEY', '5d6505709fb29c5d45cd44c9fe75ba6c913252b8')
+API_URL = environ.get('API_URL', 'https://viplinkshortx.in/api')
+WEB_NAME = environ.get('WEB_NAME', 'VipLinkShortx')
 
 #Optional
 SUPPORT_GROUP = environ.get('SUPPORT_GROUP', 'https://t.me/pandaztalks')
